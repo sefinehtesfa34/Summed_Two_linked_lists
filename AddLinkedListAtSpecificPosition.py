5908f34593f2d7574047c397b93c63a2c17230c6
sefinehtesfa34 <88246220+sefinehtesfa34@users.noreply.github.com>
2021-11-14 21:27:15 -0800

    Create AddLinkedListAtSpecificPosition.py
    HackerRanK challenge

---
+++ AddLinkedListAtSpecificPosition.py
@@ -0,0 +1,71 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+class SinglyLinkedListNode:
+    def __init__(self, node_data):
+        self.data = node_data
+        self.next = None
+
+class SinglyLinkedList:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def insert_node(self, node_data):
+        node = SinglyLinkedListNode(node_data)
+
+        if not self.head:
+            self.head = node
+        else:
+            self.tail.next = node
+
+
+        self.tail = node
+
+def print_singly_linked_list(node, sep, fptr):
+    while node:
+        fptr.write(str(node.data))
+
+        node = node.next
+
+        if node:
+            fptr.write(sep)
+
+#
+# Complete the 'insertNodeAtPosition' function below.
+#
+# The function is expected to return an INTEGER_SINGLY_LINKED_LIST.
+# The function accepts following parameters:
+#  1. INTEGER_SINGLY_LINKED_LIST llist
+#  2. INTEGER data
+#  3. INTEGER position
+#
+
+#
+# For your reference:
+#
+# SinglyLinkedListNode:
+#     int data
+#     SinglyLinkedListNode next
+#
+#
+
+def insertNodeAtPosition(llist, data, position):
+    temp=llist
+    node=SinglyLinkedListNode(data)
+    prev=None
+    while position>=1:
+        prev=temp
+        temp=temp.next
+        position-=1
+    node.next=temp
+    prev.next=node
+    return llist
+    # Write your code here
+
+if __name__ == '__main__':
